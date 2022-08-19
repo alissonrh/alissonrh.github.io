b0e7140baa31102ac53e958d433889c601edcc2e
alissonRH <alisson.honorato@live.com>
2022-08-19 00:08:33 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and sa
+          Edit <code>ALISSON</code> and sa
         </p>
         <a
           className="App-link"
