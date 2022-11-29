f5f1062bf1aa1965a3f1e5ef74d42f77f5cdc993
alissonRH <alisson.honorato@live.com>
2022-11-28 21:26:41 -0300

    logo ferramentas

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,49 +1,52 @@
 import React from 'react';
 import './experience.css';
-import { BiCheckDouble } from 'react-icons/bi'
+/* import { BiCheckDouble } from 'react-icons/bi' */
 
 const Experience = () => {
   return (
     <section id='experience'>
       <h2>Experiência</h2>
-     
+
       <div className='container experience_container'>
         <div className="experience_frontend">
           <h3>Desenvolvimento Front End</h3>
           <div className="experience_content">
             <article className="experience_details">
-              <BiCheckDouble className="detais-icon" />
+              {/*          <BiCheckDouble className="detais-icon" /> */}
               <div>
-                <h4>HTML</h4>
-                <small className='text-light'></small>
+                <img align="center" alt="HTML" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"></img>
+                <h4 align="center" >HTML</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className="detais-icon" />
               <div>
-                <h4>CSS</h4>
-                <small className='text-light'></small>
+                <img align="center" alt="CSS" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"></img>
+                <h4 align="center" >CSS</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className="detais-icon" />
               <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Linguagem</small>
+                <img align="center" alt="JavaScript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></img>
+                <h4 align="center" >JavaScript</h4>
+              </div>
+
+            </article>
+            <article className="experience_details">
+              <div>
+                <img align="center" alt="TAILWIND" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"></img>
+                <h4 align="center" >Tailwind</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className="detais-icon" />
               <div>
-                <h4>Tailwind</h4>
-                <small className='text-light'>Freamework</small>
+                <img align="center" alt="REACT" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"></img>
+                <h4 align="center" >React</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className="detais-icon" />
               <div>
-                <h4>React</h4>
-                <small className='text-light'>Freamework</small>
+                <img align="center" alt="REDUX" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"></img>
+                <h4 align="center" >Redux</h4>
               </div>
             </article>
           </div>
@@ -53,51 +56,51 @@ const Experience = () => {
           <h3>Desenvolvimento Back End</h3>
           <div className="experience_content">
             <article className="experience_details">
-              <BiCheckDouble className='detais-icon' />
               <div>
-                <h4>Node.js</h4>
-                <small className='text-light'>JavaScript fora do navegador web</small>
+                <img align="center" alt="NODE" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"></img>
+                <h4 align="center" >Node.js</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className='detais-icon' />
               <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Banco de dados não-relacional</small>
+                <img align="center" alt="express" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+                <h4 align="center">Express.js</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className='detais-icon' />
               <div>
-                <h4>MySQL</h4>
-                <small className='text-light'>Banco de dados relacional</small>
+                <img align="center" alt="mongodb" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+
+                <h4 align="center" >MongoDB</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className='detais-icon' />
               <div>
-                <h4>Express.js</h4>
-                <small className='text-light'>Framework para Node.js</small>
+
+                <img align="center" alt="mysql" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" />
+
+                <h4 align="center">MySQL</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <div>
+                <img align="center" alt="Docker" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" />
+                <h4 align="center" >Docker</h4>
               </div>
             </article>
             <article className="experience_details">
-              <BiCheckDouble className="detais-icon" />
               <div>
-                <h4>Sequelize</h4>
-                <small className='text-light'>Object-relational mapping (ORM)</small>
-              </div>
-            </article>
-            <article className="experience_details">
-              <BiCheckDouble className="detais-icon" />
-              <div>
-                <h4>TypeScript</h4>
-                <small className='text-light'>Linguagem</small>
+
+                <img lign="center" alt="typescrpit" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
+
+                <h4 align="center" >TypeScript</h4>
               </div>
             </article>
           </div>
         </div>
       </div>
-      <h3 className='frase'>Conheça todas minhas ferramentas acessando meu <a href='https://github.com/alissonrh' target='_blanck'>GitHub</a></h3>
+      <h3 className='frase'>Conheça todas as minhas habilidades acessando meu <a href='https://github.com/alissonrh' target='_blanck'>GitHub</a></h3>
     </section>
   )
 }
