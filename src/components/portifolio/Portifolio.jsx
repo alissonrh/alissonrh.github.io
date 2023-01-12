190914b83d9dd75cbdcc604ced6eb28d0c29ad07
alissonRH <alisson.honorato@live.com>
2023-01-12 12:54:15 -0300

    atualizei portfolio

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -11,55 +11,58 @@ const Portifolio = () => {
       <h2>Portfólio</h2>
 
       <div className='center'>
-      <div className="container_buttons">
-        <button
-          type="button"
-          onClick={() => setFilter('all')}
-        >
-          Todos
-        </button>
-        <button
-          type="button"
-
-          onClick={() => setFilter('fullstack')}
-        >
-          Full Stack
-        </button>
-        <button
-          type="button"
-          onClick={() => setFilter('frontend')}
-        >
-          Front-End
-        </button>
-        <button
-          type="button"
-          onClick={() => setFilter('backend')}
-        >
-          Back-End
-        </button>
+        <div className="container_buttons">
+          <button
+            className={filter === 'all' ? 'active' : ''}
+            type="button"
+            onClick={() => setFilter('all')}
+          >
+            Todos
+          </button>
+          <button
+            className={filter === 'fullstack' ? 'active' : ''}
+            type="button"
+            onClick={() => setFilter('fullstack')}
+          >
+            Full Stack
+          </button>
+          <button
+            className={filter === 'frontend' ? 'active' : ''}
+            type="button"
+            onClick={() => setFilter('frontend')}
+          >
+            Front-End
+          </button>
+          <button
+            className={filter === 'backend' ? 'active' : ''}
+            type="button"
+            onClick={() => setFilter('backend')}
+          >
+            Back-End
+          </button>
+        </div>
       </div>
-      </div>
 
-      
+
 
       <div className="container portfolio_container">
         {
 
           data.filter((projeto) => (filter === 'all' ? projeto : projeto.type === filter))
-          .map(({ id, image, title, github, link }) => {
-            return (
-              <article key={id} className='portfolio_items'>
-                <div className='portfolio_item-image' >
-                  <img src={image} alt='img' />
-                </div>
-                <h3>{title}</h3>
-                <div className='portfolio_item-cta' >
-                  <a href={github} rel="noreferrer" className='btn' target='_blank'>GitHub</a>
-                  {link === '' ? null : <a href={link} rel="noreferrer" className='btn btn-primary' target='_blank'>Link</a>}
-                </div>
-              </article>
-            )
-          })
+            .map(({ id, image, title, github, link }) => {
+              return (
+                <article key={id} className='portfolio_items'>
+                  <div className='portfolio_item-image' >
+                    <img src={image} alt='img' />
+                  </div>
+                  <h3>{title}</h3>
+                  <div className='portfolio_item-cta' >
+                    <a href={github} rel="noreferrer" className='btn' target='_blank'>GitHub</a>
+                    {link === '' ? null : <a href={link} rel="noreferrer" className='btn btn-primary' target='_blank'>Link</a>}
+                  </div>
+                </article>
+              )
+            })
         }
       </div>
 
