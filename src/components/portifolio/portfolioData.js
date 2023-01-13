0fe1249d9543d01ee9858fafb7a4e1bd3cac1b9a
alissonRH <alisson.honorato@live.com>
2023-01-13 18:06:03 -0300

    adicionei o solar system

--- src/components/portifolio/portfolioData.js
+++ src/components/portifolio/portfolioData.js
@@ -3,6 +3,7 @@ import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
 import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.png'
 import API from '../../assets/api.jpg'
 
 const data = [
@@ -29,6 +30,14 @@ const data = [
     github: 'https://github.com/alissonrh/tfc',
     link: 'https://tfc-vercel.vercel.app/leaderboard',
     type: 'fullstack'
+  },
+  {
+    id: 8,
+    image: IMG6,
+    title: 'Projeto Solar System',
+    github: 'https://github.com/alissonrh/solar-system',
+    link: 'https://alissonrh.github.io/solar-system/',
+    type: 'frontend'
   },
   {
     id: 2,
