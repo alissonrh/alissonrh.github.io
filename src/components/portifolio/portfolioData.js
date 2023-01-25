03e175be193b6914bbbffd5d94707f3deb4aad35
alissonRH <alisson.honorato@live.com>
2023-01-25 12:02:34 -0300

    botei mais trabs no port

--- src/components/portifolio/portfolioData.js
+++ src/components/portifolio/portfolioData.js
@@ -4,25 +4,10 @@ import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.png'
+import IMG9 from '../../assets/portfolio7.png'
 import API from '../../assets/api.jpg'
 
 const data = [
-  {
-    id: 7,
-    image: IMG5,
-    title: 'App de Delivery',
-    github: 'https://github.com/alissonrh/project-delivery-app',
-    link: '',
-    type: 'fullstack'
-  },
-  {
-    id: 6,
-    image: IMG4,
-    title: 'React Map App',
-    github: 'https://github.com/alissonrh/map-app',
-    link: 'https://react-map-alisson.netlify.app/',
-    type: 'fullstack'
-  },
   {
     id: 1,
     image: IMG1,
@@ -30,14 +15,6 @@ const data = [
     github: 'https://github.com/alissonrh/tfc',
     link: 'https://tfc-vercel.vercel.app/leaderboard',
     type: 'fullstack'
-  },
-  {
-    id: 8,
-    image: IMG6,
-    title: 'Projeto Solar System',
-    github: 'https://github.com/alissonrh/solar-system',
-    link: 'https://alissonrh.github.io/solar-system/',
-    type: 'frontend'
   },
   {
     id: 2,
@@ -70,7 +47,47 @@ const data = [
     github: 'https://github.com/alissonrh/store-manager',
     link: '',
     type: 'backend'
-  }
+  },
+  {
+    id: 6,
+    image: IMG4,
+    title: 'React Map App',
+    github: 'https://github.com/alissonrh/map-app',
+    link: 'https://react-map-alisson.netlify.app/',
+    type: 'fullstack'
+  },
+  {
+    id: 7,
+    image: IMG5,
+    title: 'App de Delivery',
+    github: 'https://github.com/alissonrh/project-delivery-app',
+    link: '',
+    type: 'fullstack'
+  },
+  {
+    id: 8,
+    image: IMG6,
+    title: 'Projeto Solar System',
+    github: 'https://github.com/alissonrh/solar-system',
+    link: 'https://alissonrh.github.io/solar-system/',
+    type: 'frontend'
+  },
+  {
+    id: 9,
+    image: IMG9,
+    title: 'Projeto Solar System',
+    github: 'https://github.com/alissonrh/starwars-planets-search',
+    link: 'https://starwars-planets-search-alissonrh.vercel.app/',
+    type: 'frontend'
+  },
+  {
+    id: 10,
+    image: API,
+    title: 'Car Shop TS',
+    github: 'https://github.com/alissonrh/car-shop-ts',
+    link: '',
+    type: 'backend'
+  },
 ];
 
 export default data
